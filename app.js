a9a02f2e5c3d3a95bdbafde8cca294fbd0b457e3
Opart Yongnoo <opartyongnoo@gmail.com>
2022-03-31 22:26:29 +0700

    fix log output

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const fs = require('fs');
  * Given a URL, get the page details from the page
  * @template T
  * @param {string} [url] - The URL of the page to scrape.
- * @param {(log: string) => T} logCallback - A function that will be called with a string parameter.
+ * @param {(log: {logType?: string, url?: string, urlImage?: string, pageNumber?: number, imageNumber?: number, descriptions?: descriptions}) => T} logCallback - A function that will be called with a string parameter.
  * @returns {url: string; pageDetails: Array<{page: string, url: string}>;}
  */
 const getPageDetails = async (url = '', logCallback) => {
@@ -38,17 +38,24 @@ const getPageDetails = async (url = '', logCallback) => {
         .filter(where => where);
 
     if (logCallback) {
-        logCallback(`${url} : contains ${pageDetails.length} pages`);
+        logCallback(
+            {
+                logType: 'info',
+                url: url,
+                descriptions: `contains ${pageDetails.length} pages`
+            }
+        );
     }
 
     return { url, pageDetails };
 };
 
+
 /**
  * Get the gallery details from the page
  * @template T
  * @param {string} [url] - The url of the page you want to scrape.
- * @param {(log: string) => T} logCallback - A function that will be called with a string parameter.
+ * @param {(log: {logType?: string, url?: string, urlImage?: string, pageNumber?: number, imageNumber?: number, descriptions?: descriptions}) => T} logCallback - A function that will be called with a string parameter.
  * @returns {Promise<{url: string; pageGalleryDetails: string[];}>} an object with the url and the array of the image urls.
  */
 const getPageGelleryDetails = async (url = '', logCallback) => {
@@ -72,7 +79,13 @@ const getPageGelleryDetails = async (url = '', logCallback) => {
         .filter(where => where);
 
     if (logCallback) {
-        logCallback(`${url} : contains ${pageGalleryDetails.length} images`);
+        logCallback(
+            {
+                logType: 'info',
+                url: url,
+                descriptions: `contains ${pageGalleryDetails.length} images`
+            }
+        );
     }
 
     return { url, pageGalleryDetails };
@@ -83,7 +96,7 @@ const getPageGelleryDetails = async (url = '', logCallback) => {
  * Get the image url from the page
  * @template T
  * @param {string} [url] - The URL of the image.
- * @param {(log: string) => T} logCallback - A function that will be called with the log message.
+ * @param {(log: {logType?: string, url?: string, urlImage?: string, pageNumber?: number, imageNumber?: number, descriptions?: descriptions}) => T} logCallback - A function that will be called with the log message.
  * @returns {Promise<string>} The image source.
  */
 const getImageDetail = async (url = '', logCallback) => {
@@ -102,12 +115,17 @@ const getImageDetail = async (url = '', logCallback) => {
     const imageAttrSrc = $('#img').attr("src");
 
     if (logCallback) {
-        logCallback(`${url} : detect #img.src`);
+        logCallback(
+            {
+                logType: 'info',
+                url: url,
+                descriptions: `detect #img.src`
+            }
+        );
     }
 
     return imageAttrSrc;
 };
-
 
 
 /**
@@ -116,7 +134,7 @@ const getImageDetail = async (url = '', logCallback) => {
  * @param {string} [url] - The URL of the image to download.
  * @param {string} downloadFolder - The folder where the image will be downloaded.
  * @param {string} [fileName] - The name of the file to be saved.
- * @param {(log: string) => T} logCallback - A function that will be called with the local file path of the downloaded
+ * @param {(log: {logType?: string, url?: string, urlImage?: string, pageNumber?: number, imageNumber?: number, descriptions?: descriptions}) => T} logCallback - A function that will be called with the local file path of the downloaded
  * image.
  * @returns The file path to the downloaded image.
  */
@@ -152,7 +170,13 @@ const downloadImage = async (url = '', downloadFolder, fileName = Date.now().toS
     });
 
     if (logCallback) {
-        logCallback(wr);
+        logCallback(
+            {
+                logType: 'info',
+                url: url,
+                descriptions: `write file is complete at path ${wr}`
+            }
+        );
     }
 
     return wr;
@@ -166,7 +190,7 @@ const downloadImage = async (url = '', downloadFolder, fileName = Date.now().toS
  * @param {string} [urlImage] - The URL of the image to download.
  * @param {string} [downloadFolder] - The folder where the images will be downloaded.
  * @param {number} [retryDownload=5] - The number of retry to download the image.
- * @param {(log: string) => T} logCallback - A function that will be called with the downloaded image path.
+ * @param {(log: {logType?: string, url?: string, urlImage?: string, pageNumber?: number, imageNumber?: number, descriptions?: descriptions}) => T} logCallback - A function that will be called with the downloaded image path.
  * @returns {Promise<string>} The image path.
  */
 const handleDownloadImage = async (imageNumber = 0, urlImage = '', downloadFolder = '', retryDownload = 5, logCallback) => {
@@ -177,12 +201,28 @@ const handleDownloadImage = async (imageNumber = 0, urlImage = '', downloadFolde
         const downloadedImage = await downloadImage(urlImage, downloadFolder, String(imageNumber).padStart(3, '0'))
             .catch(async () => {
                 if (logCallback) {
-                    logCallback(`image_number ${imageNumber} : url ${urlImage} : Retry download (${retryDownload})`);
+                    logCallback(
+                        {
+                            logType: 'warning',
+                            imageNumber: imageNumber,
+                            url: url,
+                            descriptions: `retry download (${retryDownload})`
+                        }
+                    );
                 }
                 return await handleDownloadImage(imageNumber, urlImage, downloadFolder, retryDownload - 1, logCallback);
             });
 
-        logCallback(`image_number ${imageNumber} : url ${urlImage} : download complete at path ${downloadedImage}`);
+        if (logCallback) {
+            logCallback(
+                {
+                    logType: 'info',
+                    imageNumber: imageNumber,
+                    urlImage: urlImage,
+                    descriptions: `download complete at path ${downloadedImage}`
+                }
+            );
+        }
 
         return downloadedImage;
     }
@@ -194,7 +234,7 @@ const handleDownloadImage = async (imageNumber = 0, urlImage = '', downloadFolde
  * @template T
  * @param {string} [url] - The URL of the page to be scraped.
  * @param {Array<{page: string, url: string}>} [pageURLs] - an array of page URLs that we want to crawl.
- * @param {(log: string) => T} logCallback - A function that will be called when the script is running.
+ * @param {(log: {logType?: string, url?: string, urlImage?: string, pageNumber?: number, imageNumber?: number, descriptions?: descriptions}) => T} logCallback - A function that will be called when the script is running.
  * @returns {Promise<Array<{page: string, url: string}>>} an array of objects that contain the page and url.
  */
 const initPage = async (url = '', pageURLs = [], logCallback) => {
@@ -211,19 +251,24 @@ const initPage = async (url = '', pageURLs = [], logCallback) => {
     }
 
     if (logCallback) {
-        logCallback(`${url} init page`);
+        logCallback(
+            {
+                logType: 'info',
+                url: url,
+                descriptions: `init page`
+            }
+        );
     }
 
     return pageURLs;
 };
 
 
-
 /**
  * It downloads all the images from a given URL.
  * @template T
  * @param {string} [url] - The URL of the gallery you want to download.
- * @param {(log: string) => T} [logCallback] - A function that will be called with the log data.
+ * @param {(log: {logType?: string, url?: string, urlImage?: string, pageNumber?: number, imageNumber?: number, descriptions?: descriptions}) => T} [logCallback] - A function that will be called with the log data.
  * @returns The downloadALL is an array of promises. Each promise is a download of an image.
  */
 const downloadEH = async (url = '', logCallback = (log) => console.log({ data: log })) => {
@@ -232,7 +277,13 @@ const downloadEH = async (url = '', logCallback = (log) => console.log({ data: l
     fs.mkdirSync(downloadFolder, { recursive: true, mode: 0o777 })
     const pageLists = await initPage(url);
     if (logCallback) {
-        logCallback(`url ${url} : contains page ${pageLists.length}`);
+        logCallback(
+            {
+                logType: 'info',
+                url: url,
+                descriptions: `contains page ${pageLists.length}`
+            }
+        );
     }
     let imageNumber = 0;
 
@@ -241,7 +292,14 @@ const downloadEH = async (url = '', logCallback = (log) => console.log({ data: l
         const element = pageLists[index];
         const responseGallery = await getPageGelleryDetails(element.url);
         if (logCallback) {
-            logCallback(`url ${url} : collecting info page ${index + 1} : contain images ${responseGallery.pageGalleryDetails.length}`);
+            logCallback(
+                {
+                    logType: 'info',
+                    url: url,
+                    pageNumber: index + 1,
+                    descriptions: `contain images ${responseGallery.pageGalleryDetails.length}`
+                }
+            );
         }
         for (let idx = 0; idx < responseGallery.pageGalleryDetails.length; idx++) {
             ++imageNumber;
@@ -250,6 +308,7 @@ const downloadEH = async (url = '', logCallback = (log) => console.log({ data: l
             downloadImageLists.push({ imageNumber, urlImage });
         }
     }
+
     /**
      * A loop that will download all the images from the given URL.
      * @type {string[]}
@@ -262,7 +321,13 @@ const downloadEH = async (url = '', logCallback = (log) => console.log({ data: l
 
     const downloadALL = await Promise.all(procALL);
 
-    return downloadALL;
+    return {
+        URL: url,
+        URLPaths: [URLpaths[1], URLpaths[2]],
+        downloadDirectory: downloadFolder,
+        imageURLs: downloadALL,
+    };
 };
+
 
 module.exports = downloadEH;
